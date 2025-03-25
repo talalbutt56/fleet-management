f113ee82d2e37d515cab68cd2ebaca92e51a4dc9
talalbutt56 <talal_b56@hotmail.com>
2025-03-25 12:54:24 -0400

    app.js

--- public/app.js
+++ public/app.js
@@ -291,3 +291,8 @@ class FleetApp {
 document.addEventListener('DOMContentLoaded', () => {
   new FleetApp();
 });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
